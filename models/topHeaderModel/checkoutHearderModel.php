e205697c70e908d9ee0863fe779ae5f8f0f63f81
吴虹松 <wuhongsong@wuhongsong-PC.(none)>
2014-09-20 18:07:09 +0800

    step_4

---
+++ models/topHeaderModel/checkoutHearderModel.php
@@ -0,0 +1,30 @@
+<div class="tl_topNavMain">
+    <div class="tl_topNav clearfix">
+        <p class="fl"><a href="" class="useLink">旅游攻略首页</a>&gt;&nbsp;<a href="" class="useLink">非洲</a>&gt;&nbsp;<a href="">非洲</a></p>
+        <div class="fr"><a href=""><i class="icon icon_travels"></i>发游记</a></div>
+    </div>
+    <div class="tl_userInfo">
+        <dl>
+            <dt><div><img src="statics/image/userHeader.png" width="84" height="84"></div></dt>
+            <dd><i class="iconBig iconEssence"></i><strong class="tl_tricptitle">雨中有只小阿狗</strong><span class="tl_tag">美图</span><span class="tl_tag">实用</span><span class="tl_tag">官方</span><a href="javascript:;"><i class="icon icon_modifyTitle"></i>修改</a></dd>
+            <dd><p><strong>from sina weibo</strong> 向左转一路向西(2398200492) <span>于<time>2013-08-10</time>发布<em>|</em>10天<em>|</em>和朋友出行<em>|</em>自由行<em>|</em>浏览<i>129241</i>次</span></p></dd>
+        </dl>
+        <div class="tl_shareCollection">
+            <a href="javascript:;" class="zan"><i class="icon icon_zan"></i>赞 324</a>
+            <a href="javascript:;" class="commit"><i class="icon icon_commit"></i>评论 324</a>
+            <a href="javascript:;" class="share"><i class="icon icon_share"></i>分享 324</a>
+            <a href="javascript:;" class="favorites"><i class="icon icon_favorites"></i>收藏</a>
+        </div>
+    </div>
+    <div class="tl_shortInfo clearfix">
+        <div class="tl_userShort fl">
+            <p><i class="iconBig iconEssence"></i><strong class="tl_tricptitle">雨中有只小阿狗</strong><span class="tl_tag">美图</span><span class="tl_tag">实用</span><span class="tl_tag">官方</span><a href="javascript:;"><i class="icon icon_modifyTitle"></i>修改</a></p>
+        </div>
+        <div class="tl_shortShare fr">
+            <a href="javascript:;"><i class="icon icon_zan"></i>赞 324</a>
+            <a href="javascript:;"><i class="icon icon_commit"></i>评论 324</a>
+            <a href="javascript:;"><i class="icon icon_share"></i>分享 324</a>
+            <a href="javascript:;" class="favorites"><i class="icon icon_favorites"></i>收藏</a>
+        </div>
+    </div>
+</div>
--- tricp_look.php
+++ tricp_look.php
@@ -12,24 +12,170 @@
 <body>
 <div style="width:100%; height:170px; background:#0CF; text-align:center; font-size:100px; line-height:170px; color:#fff;">网站公用头部，开发时去掉</div>
 <div class="tl_topNavBox">
-	<div class="tl_topNavMain">
-    	<div class="tl_topNav clearfix">
-        	<p class="fl"><a href="" class="useLink">旅游攻略首页</a>&gt;&nbsp;<a href="" class="useLink">非洲</a>&gt;&nbsp;<a href="">非洲</a></p>
-            <div class="fr"><a href=""><i class="icon icon_travels"></i>发游记</a></div>
-        </div>
-        <div class="tl_userInfo">
-        	<dl>
-            	<dt><div><img src="statics/image/userHeader.png" width="84" height="84"></div></dt>
-                <dd><i class="iconBig iconEssence"></i><strong class="title">雨中有只小阿狗</strong><span class="tl_tag">美图</span><span class="tl_tag">实用</span><span class="tl_tag">官方</span><a href="javascript:;"><i class="icon icon_modifyTitle"></i>修改</a></dd>
-                <dd><p><strong>from sina weibo</strong> 向左转一路向西(2398200492) <span>于<time>2013-08-10</time>发布<em>|</em>10天<em>|</em>和朋友出行<em>|</em>自由行<em>|</em>浏览<i>129241</i>次</span></p></dd>
+	<?php require_once "models/topHeaderModel/checkoutHearderModel.php" ?>
+</div>
+<div class="t_body clearfix">
+	<div class="t_main">
+        <div class="t_mainArtical fl">
+            <p class="t_m_foreword">等不到天黑，烟火不会太完美回忆烧成灰还是等不到结尾回忆烧成灰。还是等不到结尾等不到天黑，烟火不会太完美，还是等不到结尾等不到天黑，烟火不会太完美</p>
+            <h6 class="t_dayTime">第1天<span>2013-08-10</span></h6>
+            <dl class="t_articalList">
+            	<dt>
+                	<p><i class="icon icon_views_active"></i><span>雷峰塔</span></p>
+                    <div class="t_artical_laber clearfix">
+                    	<div class="t_laberImg fl"><img src="statics/image/labelpic.png" width="200" height="135"></div>
+                        <div class="t_laberTxt fr">
+                        	<h4>灵隐寺</h4>
+                            <p>地址：浙江省杭州市</p>
+                            <p>电话：0521-56214213</p>
+                            <p><a href="">243</a>篇游记分享 | <a href="">1515</a>张精彩照片</p>
+                        </div>
+                        <i class="icon icon_arrowPosD"></i>
+                        <a href="javascript:;" class="icon t_closeLaber"></a>
+                    </div>
+                </dt>
+                <dd>
+                	<div class="t_artical">
+                    	<p>废了九牛二虎之力，我们终于平安到达旅馆。<br>房间不大，卫生间、浴室都是公用。我简单收拾了一下，就到了免费华夫饼时间。白天在汉莎的飞机上已经吃了5顿餐点，晚上这段华夫饼，真有些吃不下。可是，这种占便宜的活动，怎么能错过。到达餐厅，服务生正在烤华夫饼，新鲜出炉的华夫饼味道很赞~<br>一天的奔波，吃完华夫饼后，我们早早就休息了。一天的奔波，吃完华夫饼后，我们早早就休息了。</p>
+                        <div class="t_articalCommit pr20">
+                        	<a href="javascript:;" class="zan"><i class="icon icon_sm_zan"></i>喜欢（<em>4</em>）</a>
+                            <a href="javascript:;" class="commit"><i class="icon icon_sm_commit"></i>评论（<em>4</em>）</a>
+                            <a href="javascript:;" class="share"><i class="icon icon_sm_share"></i>分享（<em>4</em>）</a>
+                        </div>
+                        <div class="t_picCommit">
+                            <i class="icon icon_upArrow"></i>
+                            <textarea></textarea>
+                            <div class="t_subCommit">已输入<span>0</span><em>/500</em>字<a href="javascript:;">评论</a></div>
+                            <ul class="t_picComlist">
+                                <li>
+                                    <span class="t_com_user">点点fiona：</span>
+                                    <p>哇 真好 冬天的韩国更美了 尤其是飘雪的日子。哇 真好 冬天的韩国更美了 尤其是飘雪的日子哇真好冬天的韩国更美了 尤其是飘雪的日子</p>
+                                    <a href="javascript:;">回复</a>
+                                </li>
+                                <li>
+                                    <span class="t_com_user">点点fiona：</span>
+                                    <p>哇 真好 冬天的韩国更美了 尤其是飘雪的日子。哇 真好 冬天的韩国更美了 尤其是飘雪的日子哇真好冬天的韩国更美了 尤其是飘雪的日子</p>
+                                    <a href="javascript:;">回复</a>
+                                </li>
+                             </ul>
+                        </div>
+                    </div>
+                </dd>
+              <dd>
+                	<div class="t_articalPic">
+                    	<div class="icon_tagPic"><i class="icon icon_pos"></i>第1天：西湖</div>
+                    	<img src="statics/image/listpic.png" width="100%">
+                        <div class="t_articalPicTxt">
+                        	<div class="maskCorner"></div>
+                        	<p>3小时广州飞伦敦，1小时伦敦飞爱丁堡，1个半小时车程爱丁堡到圣安，抵达酒店时已经totally.3小时广州飞伦敦，1小时伦敦飞爱丁堡，1个半小时车程爱丁堡到圣安，抵达酒店时已经totally</p>
+                            <div class="t_articalCommit">
+                            	<a href="javascript:;" class="t_arcitalPoint fl"><i class="icon icon_sm_hotel"></i>雷锋塔</a>
+                            	<a href="javascript:;" class="zan"><i class="icon icon_sm_zan"></i>喜欢（<em>4</em>）</a>
+                                <a href="javascript:;" class="commit"><i class="icon icon_sm_commit"></i>评论（<em>4</em>）</a>
+                                <a href="javascript:;" class="share"><i class="icon icon_sm_share"></i>分享（<em>4</em>）</a>
+                            </div>
+                            <div class="t_picCommit">
+                           		<i class="icon icon_upArrow"></i>
+                                <textarea></textarea>
+                                <div class="t_subCommit">已输入<span>0</span><em>/500</em>字<a href="javascript:;">评论</a></div>
+                                <ul class="t_picComlist">
+                                	<li>
+                                    	<span class="t_com_user">点点fiona：</span>
+                                        <p>哇 真好 冬天的韩国更美了 尤其是飘雪的日子。哇 真好 冬天的韩国更美了 尤其是飘雪的日子哇真好冬天的韩国更美了 尤其是飘雪的日子</p>
+                                        <a href="javascript:;">回复</a>
+                                    </li>
+                                    <li>
+                                    	<span class="t_com_user">点点fiona：</span>
+                                        <p>哇 真好 冬天的韩国更美了 尤其是飘雪的日子。哇 真好 冬天的韩国更美了 尤其是飘雪的日子哇真好冬天的韩国更美了 尤其是飘雪的日子</p>
+                                        <a href="javascript:;">回复</a>
+                                    </li>
+                                 </ul>
+                            </div>
+                        </div>
+                    </div>
+              </dd>
+              <dd>
+              		<div class="t_articalEassy">
+                    	<i class="icon icon_articalEassy"></i>
+                    	<p>翻照片的过程着实是幸福的，脑海中出现着或清晰或模糊的记忆，还有着许久许久都没有联系过的人……从最早的Hellokitty手机拍的超级不清晰的照片，到英国之后买的4s，再到手机被抢之后凑合用的黑色小三星。到英国之后买的4s，再到手机被抢之后凑合用的黑色小三星。</p>
+                        <div class="t_articalCommit">
+                            <a href="javascript:;" class="t_arcitalPoint fl"><i class="icon icon_sm_catering"></i>雷锋塔</a>
+                            <a href="javascript:;" class="zan"><i class="icon icon_sm_zan"></i>喜欢（<em>4</em>）</a>
+                            <a href="javascript:;" class="commit"><i class="icon icon_sm_commit"></i>评论（<em>4</em>）</a>
+                            <a href="javascript:;" class="share"><i class="icon icon_sm_share"></i>分享（<em>4</em>）</a>
+                        </div>
+                        <div class="t_picCommit">
+                            <i class="icon icon_upArrow"></i>
+                            <textarea></textarea>
+                            <div class="t_subCommit">已输入<span>0</span><em>/500</em>字<a href="javascript:;">评论</a></div>
+                            <ul class="t_picComlist">
+                                <li>
+                                    <span class="t_com_user">点点fiona：</span>
+                                    <p>哇 真好 冬天的韩国更美了 尤其是飘雪的日子。哇 真好 冬天的韩国更美了 尤其是飘雪的日子哇真好冬天的韩国更美了 尤其是飘雪的日子</p>
+                                    <a href="javascript:;">回复</a>
+                                </li>
+                                <li>
+                                    <span class="t_com_user">点点fiona：</span>
+                                    <p>哇 真好 冬天的韩国更美了 尤其是飘雪的日子。哇 真好 冬天的韩国更美了 尤其是飘雪的日子哇真好冬天的韩国更美了 尤其是飘雪的日子</p>
+                                    <a href="javascript:;">回复</a>
+                                </li>
+                             </ul>
+                        </div>
+                    </div>
+              </dd>
             </dl>
-            <div class="tl_shareCollection">
-            	<a href="javascript:;"><i class="icon icon_zan"></i>赞 324</a>
-                <a href="javascript:;"><i class="icon icon_commit"></i>评论 324</a>
-                <a href="javascript:;"><i class="icon icon_share"></i>分享 324</a>
-                <a href="javascript:;" class="favorites"><i class="icon icon_favorites"></i>收藏</a>
-            </div>
+            <dl class="t_articalList">
+            	<dt>
+                	<p><i class="icon icon_views_active"></i><span>雷峰塔</span></p>
+                    <div class="t_artical_laber clearfix">
+                    	<div class="t_laberImg fl"><img src="statics/image/labelpic.png" width="200" height="135"></div>
+                        <div class="t_laberTxt fr">
+                        	<h4>灵隐寺</h4>
+                            <p>地址：浙江省杭州市</p>
+                            <p>电话：0521-56214213</p>
+                            <p><a href="">243</a>篇游记分享 | <a href="">1515</a>张精彩照片</p>
+                        </div>
+                        <i class="icon icon_arrowPosD"></i>
+                        <a href="javascript:;" class="icon t_closeLaber"></a>
+                    </div>
+                </dt>
+                <dd>
+                	<div class="t_articalPic">
+                    	<div class="icon_tagPic"><i class="icon icon_pos"></i>第1天：西湖</div>
+                    	<img src="statics/image/listpic.png" width="100%">
+                        <div class="t_articalPicTxt">
+                        	<div class="maskCorner"></div>
+                        	<p>3小时广州飞伦敦，1小时伦敦飞爱丁堡，1个半小时车程爱丁堡到圣安，抵达酒店时已经totally.3小时广州飞伦敦，1小时伦敦飞爱丁堡，1个半小时车程爱丁堡到圣安，抵达酒店时已经totally</p>
+                            <div class="t_articalCommit">
+                            	<a href="javascript:;" class="t_arcitalPoint fl"><i class="icon icon_sm_hotel"></i>雷锋塔</a>
+                            	<a href="javascript:;" class="zan"><i class="icon icon_sm_zan"></i>喜欢（<em>4</em>）</a>
+                                <a href="javascript:;" class="commit"><i class="icon icon_sm_commit"></i>评论（<em>4</em>）</a>
+                                <a href="javascript:;" class="share"><i class="icon icon_sm_share"></i>分享（<em>4</em>）</a>
+                            </div>
+                            <div class="t_picCommit">
+                           		<i class="icon icon_upArrow"></i>
+                                <textarea></textarea>
+                                <div class="t_subCommit">已输入<span>0</span><em>/500</em>字<a href="javascript:;">评论</a></div>
+                                <ul class="t_picComlist">
+                                	<li>
+                                    	<span class="t_com_user">点点fiona：</span>
+                                        <p>哇 真好 冬天的韩国更美了 尤其是飘雪的日子。哇 真好 冬天的韩国更美了 尤其是飘雪的日子哇真好冬天的韩国更美了 尤其是飘雪的日子</p>
+                                        <a href="javascript:;">回复</a>
+                                    </li>
+                                    <li>
+                                    	<span class="t_com_user">点点fiona：</span>
+                                        <p>哇 真好 冬天的韩国更美了 尤其是飘雪的日子。哇 真好 冬天的韩国更美了 尤其是飘雪的日子哇真好冬天的韩国更美了 尤其是飘雪的日子</p>
+                                        <a href="javascript:;">回复</a>
+                                    </li>
+                                 </ul>
+                            </div>
+                        </div>
+                    </div>
+              </dd>
+            </dl>
         </div>
+        <div class="t_sider fr">
+    	</div>
     </div>
 </div>
 </body>
